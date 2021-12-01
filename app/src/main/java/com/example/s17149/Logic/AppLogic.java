f5aa74149fbcec80cd5f59d562e459ec0beb4cf2
Filip Rajzer <rajzer.filip@gmail.com>
2021-12-01 16:23:08 +0100

    Firebase 1/3 registration, login, password update complete (buggy - sometimes crashes. why?)

--- app/src/main/java/com/example/s17149/Logic/AppLogic.java
+++ app/src/main/java/com/example/s17149/Logic/AppLogic.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.s17149.AddOrEditActivity;
 import com.example.s17149.DataBase.Product;
@@ -11,6 +15,12 @@ import com.example.s17149.DataBase.ProductViewModel;
 import com.example.s17149.MainActivity;
 import com.example.s17149.OptionsActivity;
 import com.example.s17149.ProductListActivity;
+import com.example.s17149.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,9 +48,63 @@ public class AppLogic {
             UserName = "No Data";
     public static boolean
             UserIsPublishing = false;
+
+    public static FirebaseAuth fAuth;
+
+    public static void login(ActivityMainBinding biding, MainActivity activity) {
+        FirebaseUser currentUser = fAuth.getCurrentUser();
+        if (currentUser!=null&&
+                currentUser.getEmail().equals(biding.UserNameTextField.getText().toString())){
+            currentUser
+                    .updatePassword(
+                            biding
+                                    .editTextTextPassword
+                                    .getText()
+                                    .toString());
+            Toast.makeText(activity, "Password updated to: "+
+                    biding
+                            .editTextTextPassword
+                            .getText()
+                            .toString(), Toast.LENGTH_SHORT).show();
+        }else{
+            fAuth
+                .signInWithEmailAndPassword(biding.UserNameTextField.getText().toString(), biding.editTextTextPassword.getText().toString())
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            activity.UpdateForUser(user);
+                            Toast.makeText(activity, "Authentication success. user"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(activity, "Authentication failed. Trying to register", Toast.LENGTH_SHORT).show();
+
+                            fAuth.createUserWithEmailAndPassword(biding.UserNameTextField.getText().toString(), biding.editTextTextPassword.getText().toString())
+                                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            FirebaseUser user = fAuth.getCurrentUser();
+                                            activity.UpdateForUser(user);
+                                            Toast.makeText(activity, "Registration success. user"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Toast.makeText(activity, "Registration failed.",Toast.LENGTH_SHORT).show();
+                                            activity.UpdateForUser(null);
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+        }
+    }
     //=-
     //============================================================================================== CODE
-    public static void onAppStart(Context intent) {
+    public static void onAppStart() {
 
     }
     public static void checkUserData(){
--- app/src/main/java/com/example/s17149/MainActivity.kt
+++ app/src/main/java/com/example/s17149/MainActivity.kt
@@ -7,9 +7,13 @@ import android.os.Bundle
 import android.util.TypedValue
 import com.example.s17149.Logic.AppLogic
 import com.example.s17149.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+
 
 class MainActivity : AppCompatActivity() {
-
 
     private lateinit var biding: ActivityMainBinding;
 
@@ -22,19 +26,41 @@ class MainActivity : AppCompatActivity() {
             AppLogic.sp = getPreferences(MODE_PRIVATE);
             AppLogic.spEditor = AppLogic.sp.edit();
         }
-
-        AppLogic.onAppStart(this);
-
         AppLogic.mainActivity = Intent(this,MainActivity::class.java);
         AppLogic.productListActivity = Intent(this,ProductListActivity::class.java);
         AppLogic.addOrEditActivity = Intent(this,AddOrEditActivity::class.java);
+        //AppLogic.optionsActivity = Intent(this,OptionsActivity::class.java);
 
-        AppLogic.optionsActivity = Intent(this,OptionsActivity::class.java);
+        AppLogic.fAuth = FirebaseAuth.getInstance();
+        val currentUser: FirebaseUser? = AppLogic.fAuth.getCurrentUser();
+        if(currentUser!=null)
+            UpdateForUser(currentUser!!);
     }
     fun buAc01ProductList(view: android.view.View) {
         startActivity(AppLogic.productListActivity);
     }
     override fun onResume() {
         super.onResume();
+    }
+    fun LoginButton(view: android.view.View) {
+        //1-change password;2-login;3-register
+        AppLogic.login(biding,this);
+    }
+
+    /**
+     * this function updates the values (text) of email and status text fields in UI
+     * @param user user that is logged in. (or null if no user is logged in)
+     */
+    fun UpdateForUser(user:FirebaseUser?){
+        if(user!=null){
+            biding.UserNameTextField.setText(user.email);
+            biding.editTextTextPassword.setText("");
+            biding.UserIdTextView.setText("Logged in as User ${user.email} with uid: ${user.uid}");
+        }else{
+            biding.UserNameTextField.setText("");
+            biding.editTextTextPassword.setText("");
+            biding.UserIdTextView.setText("Not logged in");
+        }
+
     }
 }
